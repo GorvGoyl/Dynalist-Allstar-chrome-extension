004347601f2521563428d602280be4b04fa4f465
jerrygoyal <1gouravgg@gmail.com>
2018-02-18 14:09:59 +0530

    v0.1 stable version

---
+++ Dynalist Allstar/js/page.js
@@ -0,0 +1,8 @@
+chrome.runtime.sendMessage({ type: 'showPageAction' });
+var s = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+s.src = chrome.extension.getURL('js/inject.js');
+s.onload = function () {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
